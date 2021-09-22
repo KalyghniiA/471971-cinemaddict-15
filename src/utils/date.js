2c126824ad7e9a23faeb3c5134e9a287934e9522
Александр <kalyghnii@gmail.com>
2021-09-22 21:04:59 +0500

    Исправления

--- src/utils/date.js
+++ src/utils/date.js
@@ -41,6 +41,3 @@ export const getTimeFromMinutes = (mins) => {
 
 };
 
-export  const sortByDate = (prev, next) => {
-    Date.parse(prev.filmInfo.release.date) - Date.parse(next.filmInfo.release.date);
-}
